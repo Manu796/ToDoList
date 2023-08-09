c4b9e81047f017b9ea8f28932a75cf39b1f3cb50
Manuel Ortiz <manuel.ortiz796@gmail.com>
2023-08-09 17:22:52 -0300

    Se corrigieron funciones
    Funcionalidades con la version de Alert finalizadas.

--- js/main.js
+++ js/main.js
@@ -5,33 +5,28 @@ let listado = document.getElementById('listado') //traigo div listado
 let bienvenida = document.getElementById('bienvenida') //traigo div bienvenida
 let cerrarModal = document.getElementById('cerrarModal') // btn cerrar modal
 let item = document.querySelectorAll('.item') //traigo todos los div con clase item
-
 let check = document.getElementById('check').innerHTML //traigo check
-let tarea = document.getElementById('tarea').innerHTML //traigo tarea
+let tarea1 = document.getElementById('tarea1').innerHTML //traigo tarea
 
-let nombreIngresado = nombre.value
+let nombreIngresado = inputNombre.value
 //Ingresa nombre y al dar click en boton ingresar desaparece pantalla inicial y aparece el listado
 ingresar.addEventListener('click', () => {
     listado.style.display = 'block'
     bienvenida.style.display = 'none'
 })
 
-// item.forEach(tarea,()=>{
-
-// })
-
-
-//Al hacer click en el checkbox tacha la tarea pendiente
-document.addEventListener('click', (e) => {
-    if (e.target.id == 'check') {
-        popupContenedor.style.display = 'block'
-        // let saludo = "Bien " + nombreIngresado + "!! Terminaste de " + tarea
-        // var nuevoP = document.createElement('p')
-        // var txt = document.createTextNode(saludo)
-        // nuevoP.appendChild(txt)
-        // tarea.appendChild(p)
-    }
+item.forEach(task =>{
+    task.addEventListener("click", ()=>{
+        const label = task.querySelector('label')
+        const tituloTarea = label.innerHTML
+        
+        alertUser(nombre, tituloTarea)
+    })
 })
+
+function alertUser(nombre, tituloTarea){
+    alert(`Bien ${nombre.value}! terminaste de ${tituloTarea} !!`)
+}
 
 //Al hacer click en boton cerrar modal se cierra
 document.addEventListener('click', (e) => {
@@ -39,3 +34,20 @@ document.addEventListener('click', (e) => {
         popupContenedor.style.display = 'none'
     }
 })
+
+
+
+
+//Al hacer click en el checkbox tacha la tarea pendiente
+// document.addEventListener('click', (e) => {
+//     if (e.target.id == 'check') {
+//         // popupContenedor.style.display = 'block'
+//         // alert(`${nombreIngresado} terminaste: ${tarea1}`)
+
+//         // let saludo = "Bien " + nombreIngresado + "!! Terminaste de " + tarea
+//         // var nuevoP = document.createElement('p')
+//         // var txt = document.createTextNode(saludo)
+//         // nuevoP.appendChild(txt)
+//         // tarea.appendChild(p)
+//     }
+// })
